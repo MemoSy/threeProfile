e595425330790c0acbea23867dd09b85ad7cd6bc
MemoSy <ahmeddalhalabi1@gmail.com>
2024-11-09 12:08:51 +0300

    last push

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@
 // @ts-nocheck
 
 import dynamic from 'next/dynamic'
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
+import Navbar from "@/Components/Navbar"
+import Footer from "@/Components/Footer"
 
 // Dynamically import components that might use Three.js
 // with ssr disabled
-const Hero = dynamic(() => import("@/components/Hero"), { ssr: false })
-const About = dynamic(() => import("@/components/About"), { ssr: false })
-const Projects = dynamic(() => import("@/components/Projects"), { ssr: false })
-const Clients = dynamic(() => import("@/components/Clients"), { ssr: false })
-const Contacts = dynamic(() => import("@/components/Contacts"), { ssr: false }) 
+const Hero = dynamic(() => import("@/Components/Hero"), { ssr: false })
+const About = dynamic(() => import("@/Components/About"), { ssr: false })
+const Projects = dynamic(() => import("@/Components/Projects"), { ssr: false })
+const Clients = dynamic(() => import("@/Components/Clients"), { ssr: false })
+const Contacts = dynamic(() => import("@/Components/Contacts"), { ssr: false }) 
 
 export default function Home() {
   return (
